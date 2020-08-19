f6bd0e303995353ffba89c8f8d3adee3263f02c3
Chris Tsui <chris.tsui112@outlook.com>
2020-08-19 06:01:09 -0700

    created init function

--- code.js
+++ code.js
@@ -3,14 +3,22 @@ let data = {expense:0,
             location:"",
             description:""
 };
-let inputNodeList = document.querySelectorAll("input");
-let button = document.getElementById("submit");
-let form = document.querySelector('form')
-form.addEventListener('submit',event=> {
-    data.expense = document.form1.expense.value;
-    data.date = document.form1.date.value;
-    data.location = document.form1.location.value;
-    data.description = document.form1.description.value;
-    console.log(data)
-    event.preventDefault();
-});
+
+window.onload=init;
+function inputData(){
+
+}
+function init(){
+    let inputNodeList = document.querySelectorAll("input");
+    let button = document.getElementById("submit");
+    let form = document.querySelector('form');
+
+    form.addEventListener('submit',event=> {
+        data.expense = document.form1.expense.value;
+        data.date = document.form1.date.value;
+        data.location = document.form1.location.value;
+        data.description = document.form1.description.value;
+        event.preventDefault();
+    })
+}
+
